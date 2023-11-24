75a843a4ffd94d7060c6068c4a449a8bd3f0d64a
Rachel <rachelai@mit.edu>
2023-11-23 20:08:36 -0500

    create class form

--- server/concepts/class.ts
+++ server/concepts/class.ts
@@ -25,10 +25,11 @@ export default class ClassConcept {
   }
 
   async createClass(user: ObjectId, className: string) {
-    const joinCode = Math.random().toString(36).substring(2, 5);
+    const joinCode = Math.random().toString(36).substring(2, 7);
+    // const joinCode = await this.createUniqueJoinCode();
     const _id = await this.classes.createOne({ className, joinCode });
     await this.addInstructor(_id, user);
-    return { msg: "Class created successfully!", collection: await this.classes.readOne({ _id }) };
+    return { msg: "Class created successfully!", class: await this.classes.readOne({ _id }) };
   }
 
   async getClassById(classId: ObjectId, user: ObjectId) {
@@ -130,6 +131,14 @@ export default class ClassConcept {
       throw new NotFoundError("Collection not found!");
     }
   }
+
+  // private async createUniqueJoinCode() {
+  //   let joinCode = Math.random().toString(36).substring(2, 7);
+  //   while ((await this.classes.readOne({ joinCode })) == null) {
+  //     joinCode = Math.random().toString(36).substring(2, 7);
+  //   }
+  //   return joinCode;
+  // }
 
   private async getClassByJoinCode(joinCode: string) {
     const classDoc = await this.classes.readOne({ joinCode });
