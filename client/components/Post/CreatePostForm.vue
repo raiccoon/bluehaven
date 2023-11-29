e929e0088cc4ca4c692aad005e88fef1df1cca0e
Jaclyn Thi <jthi@mit.edu>
2023-11-28 21:28:59 -0500

    Fixed accidental duplication

--- client/components/Post/CreatePostForm.vue
+++ client/components/Post/CreatePostForm.vue
@@ -5,17 +5,12 @@ import { fetchy } from "../../utils/fetchy";
 const content = ref("");
 const imageLink = ref("");
 const videoLink = ref("");
-const imageLink = ref("");
-const videoLink = ref("");
 const emit = defineEmits(["refreshPosts"]);
 
 const createPost = async (content: string, imageLink: string, videoLink: string) => {
   // put back ? and check if undefined here or set as empty string, then check for empty string
-const createPost = async (content: string, imageLink: string, videoLink: string) => {
-  // put back ? and check if undefined here or set as empty string, then check for empty string
   try {
     await fetchy("/api/posts", "POST", {
-      body: { content, imageLink, videoLink },
       body: { content, imageLink, videoLink },
     });
   } catch (_) {
@@ -31,7 +26,6 @@ const emptyForm = () => {
 </script>
 
 <template>
-  <form @submit.prevent="createPost(content, imageLink, videoLink)">
   <form @submit.prevent="createPost(content, imageLink, videoLink)">
     <label for="content">Post Contents:</label>
     <textarea id="media-link" v-model="imageLink" placeholder="Paste link to image media here!"> </textarea>
