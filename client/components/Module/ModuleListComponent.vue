6dfdafde03b978b4fb330e8d8719245a1f2bbd4f
Rachel <rachelai@mit.edu>
2023-12-03 14:38:17 -0500

    wip

---
+++ client/components/Module/ModuleListComponent.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import ModuleComponent from "@/components/Module/ModuleComponent.vue";
+import { fetchy } from "@/utils/fetchy";
+import { onBeforeMount, ref } from "vue";
+
+const loaded = ref(false);
+const props = defineProps(["classId"]);
+let modules = ref<Array<Record<string, string>>>([]);
+// const displayMsg = ref(false);
+// const msg = ref("message");
+
+const getModulesInClass = async () => {
+  let moduleResults;
+  try {
+    moduleResults = await fetchy(`/api/classes/id/${props.classId}/modules`, "GET");
+  } catch (_) {
+    return;
+  }
+  modules.value = moduleResults;
+};
+
+onBeforeMount(async () => {
+  await getModulesInClass();
+  loaded.value = true;
+});
+</script>
+
+<template>
+  <section class="modules" v-if="loaded && modules.length !== 0">
+    <article v-for="module in modules" :key="module._id">
+      <ModuleComponent :module="module" />
+    </article>
+  </section>
+</template>
+
+<style scoped>
+@import "@/assets/boilerplate.css";
+</style>
