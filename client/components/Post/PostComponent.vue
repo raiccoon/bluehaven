09b6b1b0b593fd187dc23b27fd3e58a2ca64f5be
Jaclyn Thi <jthi@mit.edu>
2023-12-07 18:20:43 -0500

    Fixed issue with deleting posts pushing to Home page instead of class

--- client/components/Post/PostComponent.vue
+++ client/components/Post/PostComponent.vue
@@ -55,8 +55,8 @@ onBeforeMount(async () => {
     <menu>
       <!-- expand button takes you to the large post view -->
       <li><button class="btn-small pure-button" @click="expandPost">Expand</button></li>
-      <li><button v-if="props.post.author == currentUsername" class="btn-small pure-button" @click="emit('editPost', props.post._id)">Edit</button></li>
-      <li><button v-if="props.post.author == currentUsername" class="button-error btn-small pure-button" @click="deletePost">Delete</button></li>
+      <!-- <li><button v-if="props.post.author == currentUsername" class="btn-small pure-button" @click="emit('editPost', props.post._id)">Edit</button></li>
+      <li><button v-if="props.post.author == currentUsername" class="button-error btn-small pure-button" @click="deletePost">Delete</button></li> -->
     </menu>
     <article class="timestamp">
       <p v-if="props.post.dateCreated !== props.post.dateUpdated">Edited on: {{ formatDate(props.post.dateUpdated) }}</p>
