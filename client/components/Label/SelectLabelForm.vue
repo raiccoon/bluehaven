e14505a20f29c36f150bfd874b60109729c68848
Jaclyn Thi <jthi@mit.edu>
2023-12-10 19:02:31 -0500

    Styled filter by label dropdown

--- client/components/Label/SelectLabelForm.vue
+++ client/components/Label/SelectLabelForm.vue
@@ -20,12 +20,39 @@ onBeforeMount(async () => {
 
 <template>
   <form v-if="loaded" @submit.prevent="emit('filterByLabel', props.postId, selectedLabel)">
+    <label for="select-label">Filter comments by label:</label>
     <select id="selectedLabel" v-model="selectedLabel" placeholder="Label">
       <option :value="undefined">View all</option>
       <option v-for="label in labelsInClass" :key="label._id" :value="label._id">{{ label.name }}</option>
     </select>
-    <input type="submit" value="Submit" />
+    <input class="submit" type="submit" value="Submit" />
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.submit {
+  display: inline-block;
+  outline: none;
+  cursor: pointer;
+  font-size: 12px;
+  line-height: 1;
+  border-radius: 500px;
+  transition-property: background-color, border-color, color, box-shadow, filter;
+  transition-duration: 0.3s;
+  border: 1px solid transparent;
+  letter-spacing: 2px;
+  min-width: 70px;
+  white-space: normal;
+  text-align: center;
+  padding: 5px 5px 5px;
+  color: #5190bbff;
+  box-shadow: inset 0 0 0 2px #d6eaf9ff;
+  background-color: white;
+  height: 40px;
+}
+
+#selectedLabel {
+  padding: 0.3em;
+  margin: 0.5em;
+}
+</style>
